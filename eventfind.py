db71b762f1084a1ffb93d422a7d44544e57badbd
isabelladeleon12 <ijdeleon@berkeley.edu>
2018-08-01 08:14:28 -0700

    getEvents finds events near given lat and lon

--- eventfind.py
+++ eventfind.py
@@ -21,15 +21,16 @@ https://www.eventbriteapi.com/v3/users/me/owned_events/?token=DBOS3WZJP4SPLZV6F4
 #TODO: create method to fetch events near a location that day
 #getEvents(location)
 
-def getEvents():
+def getEvents(lat, lon):
 	events = requests.get(
-		"https://www.eventbriteapi.com/v3/events/search/",
+		"https://www.eventbriteapi.com/v3/events/search/?location.latitude=" + str(lat) + "&location.longitude=" + str(lon),
 		headers = {
 			"Authorization": "Bearer DBOS3WZJP4SPLZV6F4BT",
 		},
 		verify = True,  # Verify SSL certificate
 	)
 	event = events.json()['events'][0]
-	print(event['name'])
+	print(event['name']['text'])
 
-getEvents()
+getEvents(37.785227, -122.416889)
+
